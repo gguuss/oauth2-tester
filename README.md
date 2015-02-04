2eb513aca9e45280c1483aeb534630351e4fda5e
Gus Class <gguuss@gmail.com>
2015-02-03 19:19:27 -0800

    Tested / works

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ This simple app lets you perform various OAuth 2.0 flows for testing from
 the command line.
 
 For testing, you can configure a client from https://console.developers.google.com
-and then can use the values from there in client_secrets.json.
+and then can use the values from there in client_secrets.json. There is also
+a test page that you can use to get exchangeable auth codes and tokens.
+
+Run `python -m SimpleHTTPServer 8000` and navigate to localhost:8000/testpage.html
+for the utility / test page.
 
 *Note* This is a work in progress and may not work at all yet.
 
--- oauth2-tester.rb
+++ oauth2-tester.rb
@@ -20,104 +20,10 @@ require 'google/api_client'
 require 'google/api_client/client_secrets'
 require 'net/https'
 require 'uri'
-use Rack::Session::Pool, :expire_after => 86400 # 1 day
 
 if (ARGV.length < 2)
   puts "Usage: oauth2-tester.rb <authorization code> <access token> <refresh token>\n\n";
 end
-
-command = ARGV[0]
-code = ARGV[1]
-token = { :access_token => ARGV[1], :refresh_token => ARGV[2] }
-
-if (command == "code")
-  codeExchange
-end
-
-if (command == "call")
-  apiCall
-end
-
-
-# Configuration
-# See the README.md for getting the OAuth 2.0 client ID and
-# client secret.
-
-# Configuration that you probably don't have to change
-APPLICATION_NAME = 'OAuth 2.0 tester'
-
-# Build the global client
-$credentials = Google::APIClient::ClientSecrets.load
-$authorization = Signet::OAuth2::Client.new(
-    :authorization_uri => $credentials.authorization_uri,
-    :token_credential_uri => $credentials.token_credential_uri,
-    :client_id => $credentials.client_id,
-    :client_secret => $credentials.client_secret,
-    :redirect_uri => $credentials.redirect_uris.first,
-    :scope => PLUS_LOGIN_SCOPE)
-$client = Google::APIClient.new
-
-
-##
-# Perform OAuth 2.0 code exchange for refresh token.
-def codeExchange
-  # Upgrade the code into a token object.
-  $authorization.code = code
-  $authorization.fetch_access_token!
-  $client.authorization = $authorization
-
-  # TODO: move into verify_token function
-  id_token = $client.authorization.id_token
-  encoded_json_body = id_token.split('.')[1]
-
-  # Base64 must be a multiple of 4 characters long, trailing with '='
-  encoded_json_body += (['='] * (encoded_json_body.length % 4)).join('')
-  json_body = Base64.decode64(encoded_json_body)
-  body = JSON.parse(json_body)
-
-  # You can read the Google user ID in the ID token.
-  # "sub" represents the ID token subscriber which in our case
-  # is the user ID. This sample does not use the user ID.
-  gplus_id = body['sub']
-  puts "User ID: ${gplus_id}"
-
-  # Serialize and store the token in the user's session.
-  token_pair = TokenPair.new
-  token_pair.update_token!($client.authorization)
-
-  puts token_pair
-end
-
-
-##
-# An Example API call, list the people the user shared with this app.
-def apiCall
-  # Authorize the client and construct a Google+ service.
-  $client.authorization.update_token!(token)
-  plus = $client.discovered_api('plus', 'v1')
-
-  # Get the list of people as JSON and return it.
-  response = $client.execute!(plus.people.get, :userId => 'me').body
-  puts response
-end
-
-
-##
-# Disconnect the user by revoking the stored token and removing session objects.
-def disconnect
-  # You could reset the state at this point, but as-is it will still stay unique
-  # to this user and we're avoiding resetting the client state.
-  # session.delete(:state)
-  session.delete(:token)
-
-  # Send the revocation request and return the result.
-  revokePath = 'https://accounts.google.com/o/oauth2/revoke?token=' + token
-  uri = URI.parse(revokePath)
-  request = Net::HTTP.new(uri.host, uri.port)
-  request.use_ssl = true
-  status request.get(uri.request_uri).code
-end
-
 
 
 ##
@@ -143,3 +49,98 @@ class TokenPair
       :issued_at => Time.at(@issued_at)}
   end
 end
+
+
+
+##
+# Perform OAuth 2.0 code exchange for refresh token.
+def codeExchange(code)
+  # Upgrade the code into a token object.
+  $authorization.code = code
+  $authorization.fetch_access_token!
+  $client.authorization = $authorization
+
+  # TODO: move into verify_token function
+  id_token = $client.authorization.id_token
+  encoded_json_body = id_token.split('.')[1]
+
+  # Base64 must be a multiple of 4 characters long, trailing with '='
+  encoded_json_body += (['='] * (encoded_json_body.length % 4)).join('')
+  json_body = Base64.decode64(encoded_json_body)
+  body = JSON.parse(json_body)
+
+  # You can read the Google user ID in the ID token.
+  # "sub" represents the ID token subscriber which in our case
+  # is the user ID. This sample does not use the user ID.
+  gplus_id = body['sub']
+  puts "User ID: "
+  puts gplus_id.inspect
+
+  # Serialize and store the token in the user's session.
+  token_pair = TokenPair.new
+  token_pair.update_token!($client.authorization)
+
+  puts token_pair.inspect
+end
+
+
+##
+# An Example API call, list the people the user shared with this app.
+def apiCall(token)
+  # Authorize the client and construct a Google+ service.
+  $client.authorization.update_token!(token)
+  plus = $client.discovered_api('plus', 'v1')
+
+  # Get the list of people as JSON and return it.
+  response = $client.execute!(plus.people.get, :userId => 'me').body
+  puts response.inspect
+end
+
+
+##
+# Disconnect the user by revoking the stored token and removing session objects.
+def disconnect(token)
+  # You could reset the state at this point, but as-is it will still stay unique
+  # to this user and we're avoiding resetting the client state.
+  # session.delete(:state)
+  session.delete(:token)
+
+  # Send the revocation request and return the result.
+  revokePath = 'https://accounts.google.com/o/oauth2/revoke?token=' + token
+  uri = URI.parse(revokePath)
+  request = Net::HTTP.new(uri.host, uri.port)
+  request.use_ssl = true
+  status request.get(uri.request_uri).code
+end
+
+# Configuration
+# See the README.md for getting the OAuth 2.0 client ID and
+# client secret.
+
+# Configuration that you probably don't have to change
+APPLICATION_NAME = 'OAuth 2.0 tester'
+
+# Build the global client
+$credentials = Google::APIClient::ClientSecrets.load
+$authorization = Signet::OAuth2::Client.new(
+    :authorization_uri => $credentials.authorization_uri,
+    :token_credential_uri => $credentials.token_credential_uri,
+    :client_id => $credentials.client_id,
+    :client_secret => $credentials.client_secret,
+    :redirect_uri => $credentials.redirect_uris.first,
+    :scope => "profile")
+$client = Google::APIClient.new
+
+
+command = ARGV[0]
+code = ARGV[1]
+token = { :access_token => ARGV[1], :refresh_token => ARGV[2] }
+
+if (command == "code")
+  codeExchange(code)
+end
+
+if (command == "call")
+  apiCall(token)
+end
+
